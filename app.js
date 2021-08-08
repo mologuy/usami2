186b0ad753a7ca536b4c67b08bba15f18bf7a807
A00820449 <mike@mologuy.com>
2021-08-07 19:13:13 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const Discord = require("discord.js");
 
+const OPTIONS = require("./options.json");
+
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] })
 
-client.login("NzkwMTUwODkzNDkxMzIyOTEw.X98bXQ.eUk56W7xhrcUSrteeD5gjrtr_BU");
+client.once("ready", async ()=>{
+    console.log("Logged in as", client.user.tag);
+});
 
-console.log("Test");
+client.on("messageCreate", async (msg)=>{
+    if (!client.application?.owner) await client.application?.fetch();
+
+	if (msg.content.toLowerCase() === '!deploy' && msg.author.id === client.application?.owner.id) {
+		const data = {
+			name: 'ping',
+			description: 'Replies with Pong!',
+		};
+
+		const command = await client.application?.commands.create(data);
+		console.log(command);
+	}
+});
+
+client.login(OPTIONS.token);
