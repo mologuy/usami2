99a6019adb59d5566715f9c7d2b3c73b15b325dd
A00820449 <mike@mologuy.com>
2021-08-11 12:41:53 -0500

    maybe fix

--- misc/dadbot.js
+++ misc/dadbot.js
@@ -11,7 +11,7 @@ async function dad(msg) {
     const avatarImg = await Jimp.read(avatarURL);
     const pipeImg = await Jimp.read("./bin/pipe.png");
 
-    avatarImg.resize(256,256).composite(pipeImg, 0, 0);
+    avatarImg.cover(256,256).composite(pipeImg, 0, 0);
 
     const image64 = await avatarImg.getBase64Async(Jimp.MIME_PNG);
 
