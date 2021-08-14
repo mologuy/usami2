fb6877b6d6b65122c03a7d643d1be7b863bd8432
A00820449 <mike@mologuy.com>
2021-08-14 18:00:56 -0500

    changed commands array to a map

--- app.js
+++ app.js
@@ -6,12 +6,23 @@ const OPTIONS = require("./options.json");
 
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] })
 
+var COMMANDS = new Map();
+var MISCS = [];
+
+function getCommandsDataArray(commandMap) {
+	var output = [];
+	commandMap.forEach((value)=>{
+		output.push(value.data);
+	});
+	return output;
+}
+
 client.once("ready", async ()=>{
 	try {
 		console.log("Logged in as", client.user.tag);
 		client.user.setActivity(`Love, love!`);
 
-		const commandsData = Object.values(COMMANDS).map((module) => module.data);
+		const commandsData = getCommandsDataArray(COMMANDS);
 		await client.guilds.cache.get(OPTIONS.main_guild_id).commands.set(commandsData);
 	}
 	catch(e) {
@@ -37,8 +48,8 @@ client.on("interactionCreate", async (interaction)=>{
 	if (!interaction.isCommand()) {return;}
 
 	try {
-		if (COMMANDS[interaction.commandName]) {
-			await COMMANDS[interaction.commandName].run(interaction);
+		if (COMMANDS.has(interaction.commandName)) {
+			await COMMANDS.get(interaction.commandName).run(interaction);
 		}
 		else {
 			await interaction.reply(`Unknown command: \`${interaction.commandName}\``);
@@ -49,9 +60,6 @@ client.on("interactionCreate", async (interaction)=>{
 	}
 });
 
-var COMMANDS = {};
-var MISCS = [];
-
 //main function
 (async function(){
 	const commFiles = await fs.readdir("./commands");
@@ -59,11 +67,11 @@ var MISCS = [];
 	for (const file of commFiles) {
 		if (file.match(/\.js$/i)) {
 			const module = require(path.join(__dirname, "commands", file));
-			if (COMMANDS[module.data.name]) {
+			if (COMMANDS.has(module.data.name)) {
 				throw new Error("Repeated command name!");
 			}
 			else {
-				COMMANDS[module.data.name] = module;
+				COMMANDS.set(module.data.name, module);
 			}
 		}
 	}
