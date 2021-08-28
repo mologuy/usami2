b5171ff43ad4feb395f9ad7b52d57aef0043f234
A00820449 <mike@mologuy.com>
2021-08-28 14:53:46 -0500

    compartmentalized webhook code

--- misc/dadbot.js
+++ misc/dadbot.js
@@ -1,14 +1,7 @@
 const Jimp = require("jimp");
 const roleId = require("../options.json").dad_ignore_role;
 
-async function dad(msg) {
-    if (msg.member.roles.cache.has(roleId)) {return;}
-
-    const dadmatch = msg.content.match(/^(i\W*m|i\s+am)\s+(.*)/i);
-    if (!dadmatch) {return;}
-    
-    const name = dadmatch[2];
-
+async function getDadHook(msg) {
     const avatarURL = msg.author.avatarURL({size: 128, format: "png"});
     
     const avatarImg = await Jimp.read(avatarURL);
@@ -17,9 +10,20 @@ async function dad(msg) {
     avatarImg.cover(128, 128).composite(pipeImg, 0, 0);
 
     const image64 = await avatarImg.getBase64Async(Jimp.MIME_PNG);
-
     const webhook = await msg.channel.createWebhook(`${msg.author.username}'s dad`, {avatar: image64});
+
+    return webhook;
+}
+
+async function dad(msg) {
+    if (msg.member.roles.cache.has(roleId)) {return;}
+
+    const dadmatch = msg.content.match(/^(i\W*m|i\s+am)\s+(.*)/i);
+
+    if (!dadmatch) {return;}
     
+    const name = dadmatch[2];
+
     let output;
     if (name.match(/^\W*dad\W*$/i)){
         output = `No, you're not. You're ${msg.author.username}!`
@@ -27,8 +31,10 @@ async function dad(msg) {
     else {
         output = `Hi, ${name}! I'm dad.`;
     }
+
+    const webhook = await getDadHook(msg);
     await webhook.send(output);
     await webhook.delete();
 }
 
-module.exports = {run: dad};
+module.exports = {run: dad, hook: getDadHook};
